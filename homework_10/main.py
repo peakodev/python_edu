339e4614b388c8b5ae959c3146d0c4ab56ab9db5
Nazar Salo <peakodev@gmail.com>
2024-01-16 13:08:29 +0200

    homework_10

---
+++ homework_10/main.py
@@ -0,0 +1,109 @@
+from collections import UserDict
+import re
+
+
+def contains_10_digit_phone_number(value):
+    pattern = r'\b\d{10}\b'
+    return bool(re.search(pattern, value))
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        if len(value) >= 3:
+            super().__init__(value)
+        else:
+            raise ValueError('The name have to be at least 3 symbols')
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if contains_10_digit_phone_number(value):
+            super().__init__(value)
+        else:
+            raise ValueError('wrong phone number')
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone_obj = Phone(phone)
+        self.phones.append(phone_obj)
+
+    def remove_phone(self, phone):
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            self.phones.remove(phone_obj)
+        else:
+            raise ValueError('Phone not found')
+
+    def edit_phone(self, phone, new_phone):
+        self.remove_phone(phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        found_phone = list(filter(lambda ph: ph.value == phone, self.phones))
+        return found_phone[0] if len(found_phone) else None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        self.data.pop(name, None)
+
+
+def main():
+    # Створення нової адресної книги
+    book = AddressBook()
+
+    # Створення запису для John
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+    book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+    for name, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+    book.delete("Jane")
+
+
+if __name__ == '__main__':
+    main()
