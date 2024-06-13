e0e2fe2978ca0f03e8b07fd6fa5e00898858107c
Nazar Salo <peakodev@gmail.com>
2024-06-13 14:56:25 +0300

    home3: factorize

--- web/homework_3/factorize.py
+++ web/homework_3/factorize.py
@@ -1,6 +1,16 @@
 import time
+import logging
 from functools import wraps
-from multiprocessing import Pool, cpu_count
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count, get_logger, log_to_stderr
+
+
+log_to_stderr()
+logger = get_logger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(processName)s : %(message)s')
+handler = logger.handlers[0]  # Стандартний обробник для log_to_stderr
+handler.setFormatter(formatter)
 
 
 def time_execution(func):
@@ -9,23 +19,28 @@ def time_execution(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Execution time for {func.__name__}: {end_time - start_time} seconds")
+        logger.info(f"Execution time for {func.__name__}: {end_time - start_time} seconds")
         return result
     return wrapper
 
 
 TEST_DATA = (128, 255, 99999, 10651060)
+EXPECTED = [
+    [1, 2, 4, 8, 16, 32, 64, 128],
+    [1, 3, 5, 15, 17, 51, 85, 255],
+    [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999],
+    [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395,
+     532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+]
 
 
-def test_factorize(a, b, c, d):
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158,
-                 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
-    print("All tests passed!")
+def test_factorize(results):
+    for result, exp in zip(results, EXPECTED):
+        assert result == exp, f"Failed test case: {result} != {exp}"
+    logger.info("All tests passed!")
 
 
+@time_execution
 def find_factors(n):
     factors = []
     for i in range(1, int(n**0.5) + 1):
@@ -38,19 +53,18 @@ def find_factors(n):
 
 @time_execution
 def factorize(*numbers):
-    print("Start factorize")
+    logger.info("Start factorize")
     return [find_factors(number) for number in numbers]
-
-
-test_factorize(*factorize(*TEST_DATA))
 
 
 @time_execution
 def factorize_multiprocessed(*numbers):
-    print("Start factorize_multiprocessed")
-    with Pool(cpu_count()) as pool:
-        result = pool.map(find_factors, numbers)
-    return result
+    logger.info("Start factorize_multiprocessed")
+    with ProcessPoolExecutor(max_workers=cpu_count()) as executor:
+        results = list(executor.map(find_factors, numbers))
+    return results
 
 
-test_factorize(*factorize_multiprocessed(*TEST_DATA))
+if __name__ == '__main__':
+    test_factorize(factorize(*TEST_DATA))
+    test_factorize(factorize_multiprocessed(*TEST_DATA))
